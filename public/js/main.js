15fa332320ed94fa40795d4d21da0a9793c2e110
deepmanupy <reddymanudeep@gmail.com>
2022-12-01 14:44:36 -0500

    using jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,64 @@
-let searchBar = document.getElementById("searchbar");
-let searchBtn = document.getElementById("searchbtn");
-let errorDiv = document.getElementById("error");
+// let searchBar = document.getElementById("searchbar");
+// let searchBtn = document.getElementById("searchbtn");
+// let errorDiv = document.getElementById("error");
 
-let searchedDevices = [];
+// let searchedDevices = [];
 
-if(searchBtn){
-    searchBtn.addEventListener('click', (event) => {
+// if(searchBtn){
+//     searchBtn.addEventListener('click', (event) => {
+//         try {
+//             event.preventDefault();
+//             //TODO: Validate the Input using Validation functions
+//             let input = searchBar.value.trim();
+//             if(!input) throw "Error: Invalid Input";
+            
+//             //TODO: After Validation route the event to getProductByName route
+
+//             //Keeping track of no.of hits on a product
+            
+//             if(!searchedDevices.find(device => device.name==input)){
+//                 searchedDevices.push({
+//                     name: input,
+//                     hit: 1
+//                 });
+//             }
+//             else{
+//                 searchedDevices.find(device => device.name==input).hit++;
+//             }
+//             errorDiv.hidden = true;
+//         } catch (error) {
+//             errorDiv.hidden = false;
+//             errorDiv.innerHTML = error;
+//         }
+//     });
+// }
+
+(function ($){
+    let searchBar = $("#searchbar");
+    let searchBtn = $("#searchbtn");
+    let errorDiv = $("#error");
+    let searchResult = $("#search-result");
+
+    searchBtn.on('click', (event) => {
         try {
             event.preventDefault();
-            //TODO: Validate the Input using Validation functions
-            let input = searchBar.value.trim();
-            if(!input) throw "Error: Invalid Input";
-            
-            //TODO: After Validation route the event to getProductByName route
+            let input = searchBar.val().trim();
+            //validation
 
-            //Keeping track of no.of hits on a product
-            
-            if(!searchedDevices.find(device => device.name==input)){
-                searchedDevices.push({
-                    name: input,
-                    hit: 1
-                });
-            }
-            else{
-                searchedDevices.find(device => device.name==input).hit++;
-            }
-            errorDiv.hidden = true;
+            let requestConfig = {
+                method: 'POST',
+                url: '/products',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    productName: input
+                })
+            };
+
+            $.ajax(requestConfig).then((responseMessage) => {
+                console.log(responseMessage);
+            });
         } catch (error) {
-            errorDiv.hidden = false;
-            errorDiv.innerHTML = error;
+            console.log(error);
         }
     });
-}
+})(window.jQuery);
