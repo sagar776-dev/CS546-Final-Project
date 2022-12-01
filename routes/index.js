ff3e0a20cbf5d7d090dc76a7a10fb3f68f2aba05
deepmanupy <reddymanudeep@gmail.com>
2022-12-01 14:44:05 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const adminRoutes = require('./admin');
 const productsRoutes = require('./products');
 const reviewsRoutes = require('./reviews');
 const usersRoutes = require('./users');
+const path = require('path');
 
 
 const constructorMethod = (app) => {
@@ -9,6 +10,10 @@ const constructorMethod = (app) => {
     app.use('/products', productsRoutes);
     app.use('/reviews', reviewsRoutes);
     app.use('/users', usersRoutes);
+
+    app.use('/', async (req, res) => {
+        res.sendFile(path.resolve('static/about.html'));
+    });
 
     app.use('*', (req, res) => {
         res.sendStatus(404);
