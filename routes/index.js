72f37db903c25ad1bad72db570e1f27928edc3a1
deepmanupy <reddymanudeep@gmail.com>
2022-12-03 19:23:52 -0500

    homepage handlebar

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const adminRoutes = require('./admin');
 const productsRoutes = require('./products');
 const reviewsRoutes = require('./reviews');
 const usersRoutes = require('./users');
-const path = require('path');
 
 
 const constructorMethod = (app) => {
@@ -12,7 +11,12 @@ const constructorMethod = (app) => {
     app.use('/users', usersRoutes);
 
     app.use('/', async (req, res) => {
-        res.sendFile(path.resolve('static/about.html'));
+        if(req.session.user){
+         return res.render('products/homepage');
+        }
+        else{
+            return res.render('users/signup');
+        }
     });
 
     app.use('*', (req, res) => {
